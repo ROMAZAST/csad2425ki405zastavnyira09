524a18903a858c1ad9bafc0b505aa6b642f55d88
Roman <100799424+ROMAZAST@users.noreply.github.com>
2024-11-27 01:23:03 +0200

    fix bat file

--- ci/CreateDocumentation.bat
+++ ci/CreateDocumentation.bat
@@ -1,14 +1,9 @@
 @echo off
 setlocal
 
-:: Отримати шлях до поточної директорії, де знаходиться скрипт
 set SCRIPT_DIR=%~dp0
 
-:: Піднятися на один рівень вгору, щоб виконати команду у батьківській директорії
 cd /d "%SCRIPT_DIR%.."
-
-:: Шлях до Doxyfile, який знаходиться в підкаталозі "доксіфайл"
-set DOXYFILE=..\Doxyfile
 
 :: Перевірка, чи існує файл Doxyfile
 if not exist "Doxyfile" (
@@ -16,11 +11,9 @@ if not exist "Doxyfile" (
     exit /b 1
 )
 
-:: Запуск Doxygen з вказаним Doxyfile
 echo Generating Doxygen documentation...
 doxygen Doxyfile
 
-:: Перевірка на помилки при виконанні Doxygen
 if %errorlevel% neq 0 (
     echo Doxygen generation failed.
     exit /b %errorlevel%
@@ -28,5 +21,4 @@ if %errorlevel% neq 0 (
 
 echo Documentation generation completed successfully.
 
-pause
 pause
